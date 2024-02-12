522f000d9af9ce65fae9184d103dddcde046532b
Ismoil2009 <osnovavbs092009@gmail.com>
2024-02-13 00:24:04 +0500

    done

---
+++ src/Data.jsx
@@ -0,0 +1,17 @@
+export const userList = [
+  {
+    name: "yousaf khan",
+    email: "yousaf1@gmail.com",
+    id: 1,
+  },
+  {
+    name: "ismoil khan",
+    email: "ismoil1@gmail.com",
+    id: 2,
+  },
+  {
+    name: "islom khan",
+    email: "islom1@gmail.com",
+    id: 3,
+  },
+];
